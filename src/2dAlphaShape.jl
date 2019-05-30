98580d3293b10972502d114749a27a2d05734544
eOnofri04 <elia.onofri@mensa.it>
2019-05-30 14:11:11 +0200

    adding manifest

--- src/2dAlphaShape.jl
+++ src/2dAlphaShape.jl
@@ -49,3 +49,78 @@ function pointsRand(V::Lar.Points, EV::Lar.Cells, n = 1000, m = 0)::Tuple{Lar.Po
 	EW = [[i] for i = 1 : m]
 	return V, W[:, 2:end], EV, EW
 end
+
+
+"""
+	alphaShape(points, EW, alpha)
+
+---
+
+# Arguments
+ - `V::Lar.Points`: The 0-cells of the original complex;
+ - `EV::Lar.Cells`: The 1-cells of the original complex;
+ - `n::Int64`: the number of new non-external points (*By default* = 1000);
+ - `m::Int64`: the number of external points (*By default* = 0);
+
+---
+
+# Examples
+```jldoctest
+julia> Vi, Ve, EVi, EVe = AlphaShape.pointsRand(V, EV, 1000, 1000);
+```
+"""
+function alphaShape(points::Lar.Points, EW::Lar.Cells, alpha=0.05)
+
+	# The following function adds the edge betwen `i` and `j` if is not present.
+	function addEdge(EV, i, j)
+
+	        if [i, j] in EV || [j, i] in EV
+	            return EV
+		end
+		return push!(EV,[i,j])
+	end
+
+
+	V = convert(Array{Float64,2},points')
+	vertices = DataStructures.OrderedDict();
+
+	#creazione del dizionario dei vertici
+    	for id in 1:size(V,1)
+       		vertices[id] =V[id,:]
+        end
+
+	@assert  size(V,1) > 3 #per usare la funzione di triangolazione devo avere almeno 4 punti
+
+	points_map = Array{Int64,1}(collect(1:1:size(V)[1]))
+	edges_list=vcat(EW'...)
+
+	triangles = Triangle.constrained_triangulation(V,points_map,edges_list)
+ 	EV = Array{Int64,1}[]
+
+	for k=1:size(triangles,1)
+		#i tre punti del triangolo
+		ia=triangles[k][1]
+		ib=triangles[k][2]
+		ic=triangles[k][3]
+
+		#gli indici associati a tre punti del triangolo
+		pa=vertices[ia]
+		pb=vertices[ib]
+		pc=vertices[ic]
+
+		#calcolo del raggio della circonferenza circostritta
+		a = sqrt((pa[1] - pb[1]) ^ 2 + (pa[2] - pb[2]) ^ 2)
+		b = sqrt((pb[1] - pc[1]) ^ 2 + (pb[2] - pc[2]) ^ 2)
+		c = sqrt((pc[1] - pa[1]) ^ 2 + (pc[2] - pa[2]) ^ 2)
+		s = (a + b + c) / 2.0
+		area = sqrt(s * (s - a) * (s - b) * (s - c))
+		circum_r = a * b * c / (4.0 * area)
+
+		if circum_r < alpha #alpha test
+		    addEdge(EV, ia, ib)
+		    addEdge(EV, ib, ic)
+		    addEdge(EV, ic, ia)
+		end
+	end
+	return convert(Lar.Points,V'),EV
+end
