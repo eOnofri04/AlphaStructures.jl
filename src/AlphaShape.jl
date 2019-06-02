abfa0cdf3c609d15c9ad07c1444a82c0694d947b
Elia Onofri <30669145+eOnofri04@users.noreply.github.com>
2019-06-02 15:59:30 +0200

    [ci skip] backtracking

--- src/AlphaShape.jl
+++ src/AlphaShape.jl
@@ -1,8 +1,4 @@
 module AlphaShape
-	if VERSION >= VersionNumber("1.0.0")
-		using Pkg
-		Pkg.add(PackageSpec(url="https://github.com/cvdlab/LinearAlgebraicRepresentation.jl", rev="julia-1.0"))
-	end
 	using LinearAlgebraicRepresentation, Triangle
 	Lar = LinearAlgebraicRepresentation
 
