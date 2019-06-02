770367996e639b77a8f024ff1d0f96175799d32d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-02 14:24:27 +0000

    Deploy eOnofri04/AlphaShape.jl to github.com/eOnofri04/AlphaShape.jl.git:gh-pages

--- search_index.js
+++ search_index.js
@@ -1,3 +1,107 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#AlphaShape.jl-1","page":"Home","title":"AlphaShape.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"AlphaShape.jl is a Julia library to ...","category":"page"},{"location":"#","page":"Home","title":"Home","text":"In this documentation it is explained a little brief of theory and how to apply it in computer science.","category":"page"},{"location":"#Dependencies-1","page":"Home","title":"Dependencies","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"AlphaShape.jl has the following dependeces:","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Plasm\nLinear Algebraic Representation","category":"page"},{"location":"#Docstrings-conventions-1","page":"Home","title":"Docstrings conventions","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Bold is used to point out theory concepts.\nMonospace is used for everything code related.","category":"page"},{"location":"#Index-1","page":"Home","title":"Index","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"gettingStarted/#Installation-1","page":"Getting Started","title":"Installation","text":"","category":"section"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"If you have not done so already, download and install Julia.","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"This package was developed with julia 1.1.0 but it should be supported also on julia 0.6.4.","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"To install AlphaShape package first you have to download this repository.","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"Then start julia and navigate to the folder called AlphaShape.jl (with the command ;cd); here run","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"include(\"src/AlphaShape.jl\")\nusing AlphaShape","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"If you want, you can test if everithyng is working fine by running","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"include(\"test/runtests.jl\")","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"this will run all the tests written so far and checking all is working properly.","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"If you want to use a Graphic Interface to preview the results you are going to build up you can also use Plasm package developed by CVD-LAB by running","category":"page"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"]add Plasm\nusing Plasm","category":"page"},{"location":"gettingStarted/#First-Steps-1","page":"Getting Started","title":"First Steps","text":"","category":"section"},{"location":"gettingStarted/#","page":"Getting Started","title":"Getting Started","text":"!!! ToDo","category":"page"},{"location":"authors/#Authors-1","page":"Authors","title":"Authors","text":"","category":"section"},{"location":"authors/#","page":"Authors","title":"Authors","text":"This repository was developed by two students at RomaTre universisty.","category":"page"},{"location":"authors/#[Elia-Onofri](https://github.com/eOnofri04)-1","page":"Authors","title":"Elia Onofri","text":"","category":"section"},{"location":"authors/#","page":"Authors","title":"Authors","text":"Ln elia.onofri4@gmail.com","category":"page"},{"location":"authors/#[Maria-Teresa-Graziano](https://github.com/marteresagh)-1","page":"Authors","title":"Maria Teresa Graziano","text":"","category":"section"},{"location":"authors/#","page":"Authors","title":"Authors","text":"marteresa28@gmail.com","category":"page"},{"location":"authors/#Mantainers-1","page":"Authors","title":"Mantainers","text":"","category":"section"},{"location":"authors/#","page":"Authors","title":"Authors","text":"This repository would be maintained by the Computational Visual Design Laboratory (CVDLAB) of Università degli Studi di Roma Tre.","category":"page"}]
-}
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#AlphaShape.jl-1",
+    "page": "Home",
+    "title": "AlphaShape.jl",
+    "category": "section",
+    "text": "AlphaShape.jl is a Julia library to ...In this documentation it is explained a little brief of theory and how to apply it in computer science."
+},
+
+{
+    "location": "index.html#Dependencies-1",
+    "page": "Home",
+    "title": "Dependencies",
+    "category": "section",
+    "text": "AlphaShape.jl has the following dependeces:Plasm\nLinear Algebraic Representation"
+},
+
+{
+    "location": "index.html#Docstrings-conventions-1",
+    "page": "Home",
+    "title": "Docstrings conventions",
+    "category": "section",
+    "text": "Bold is used to point out theory concepts.\nMonospace is used for everything code related."
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "gettingStarted.html#",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "gettingStarted.html#Installation-1",
+    "page": "Getting Started",
+    "title": "Installation",
+    "category": "section",
+    "text": "If you have not done so already, download and install Julia.This package was developed with julia 1.1.0 but it should be supported also on julia 0.6.4.To install AlphaShape package first you have to download this repository.Then start julia and navigate to the folder called AlphaShape.jl (with the command ;cd); here runinclude(\"src/AlphaShape.jl\")\nusing AlphaShapeIf you want, you can test if everithyng is working fine by runninginclude(\"test/runtests.jl\")this will run all the tests written so far and checking all is working properly.If you want to use a Graphic Interface to preview the results you are going to build up you can also use Plasm package developed by CVD-LAB by running]add Plasm\nusing Plasm"
+},
+
+{
+    "location": "gettingStarted.html#First-Steps-1",
+    "page": "Getting Started",
+    "title": "First Steps",
+    "category": "section",
+    "text": "!!! ToDo"
+},
+
+{
+    "location": "authors.html#",
+    "page": "Authors",
+    "title": "Authors",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "authors.html#Authors-1",
+    "page": "Authors",
+    "title": "Authors",
+    "category": "section",
+    "text": "This repository was developed by two students at RomaTre universisty."
+},
+
+{
+    "location": "authors.html#[Elia-Onofri](https://github.com/eOnofri04)-1",
+    "page": "Authors",
+    "title": "Elia Onofri",
+    "category": "section",
+    "text": "Ln elia.onofri4@gmail.com"
+},
+
+{
+    "location": "authors.html#[Maria-Teresa-Graziano](https://github.com/marteresagh)-1",
+    "page": "Authors",
+    "title": "Maria Teresa Graziano",
+    "category": "section",
+    "text": "marteresa28@gmail.com"
+},
+
+{
+    "location": "authors.html#Mantainers-1",
+    "page": "Authors",
+    "title": "Mantainers",
+    "category": "section",
+    "text": "This repository would be maintained by the Computational Visual Design Laboratory (CVDLAB) of Università degli Studi di Roma Tre."
+},
+
+]}
