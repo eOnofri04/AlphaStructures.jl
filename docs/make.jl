d730309f2e5332dec8961144e02f9994373e929e
eOnofri04 <elia.onofri@mensa.it>
2019-07-02 14:47:26 +0200

    Refactoring Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,15 @@ makedocs(
 	pages = [
 		"Home" => "index.md",
 		"Getting Started" => "gettingStarted.md",
-		"Introduction to Alpha Structures" => "theory.md",
-		"Authors" => "authors.md",
+		"Theory..." => [
+			"Delaunay Triangulation" => "delaunay.md",
+			"Voronoy Diagrams" => "voronoy.md",
+			"Alpha Structures" => "alpha-structures.md",
+			"Persistent Homology" => "persistent-homology.md"
+		],
+		"... and Practice" => [
+			"Module Introduction" => "this-module.md"
+		],
+		"About the Authors" => "authors.md",
 	]
 )
