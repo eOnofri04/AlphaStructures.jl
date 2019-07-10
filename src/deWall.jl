c33a642ed150457aea5a8069b9f260e9fccaf16d
eOnofri04 <elia.onofri@mensa.it>
2019-07-10 21:01:56 +0200

    Found Error (NaN in next simplex point; it takes complanar)

--- src/deWall.jl
+++ src/deWall.jl
@@ -20,6 +20,9 @@ function firstDeWallSimplex(P::Lar.Points, ax::Int64, off::Float64)
         #    dist = sum(abs2, vector, dims = 1)
         #    return findmin(dist[:])[2]
         #end
+
+		###!!!!!!!!!! NaN error
+
         findmin(
             [
                 findRadius([Psimplex P[:,col]])
@@ -33,11 +36,12 @@ function firstDeWallSimplex(P::Lar.Points, ax::Int64, off::Float64)
 
     # the first point of the simplex is the one with coordinate `ax` maximal
     #  such that it is less than `off` (closer to α from minus)
-    newidx = findmax(P[ax, findall(x -> x < off, P[ax, :])])[2]
+	Pselection = findall(x -> x < off, P[ax, :])
+    newidx = Pselection[findmax(P[ax, Pselection])[2]]
     # indices will store the indices of the simplex ...
     indices = [newidx]                      #Array{Int64,1}
     # ... and `Psimplex` will store the corresponding points
-    Psimplex = P[:, Pminor[newidx]][:,:]    #Array{Float64,2}
+    Psimplex = P[:, newidx][:,:]    #Array{Float64,2}
 
     # the second point must be seeken across those with coordinate `ax`
     #  grater than `off`
