bf9d456379e243e9f9a4d2b78054fb37cbbc45e4
marteresagh <marteresa28@gmail.com>
2021-04-13 12:12:15 +0200

    refactor code

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,62 +1,3 @@
-#===============================================================================
-#
-#	src/geometry.jl
-#
-#	This file contains:
-#
-#	 - findCenter(
-#			P::Matrix
-#		)::Array{Float64,1}
-#
-#	 - findClosestPoint(
-#			Psimplex::Matrix,
-#			P::Matrix;
-#			metric = "circumcenter"
-#		)::Union{Int64, Nothing}
-#
-#	 - findMedian(
-#			P::Matrix,
-#			ax::Int64
-#		)::Float64
-#
-#	 - findRadius(
-#			P::Matrix,
-#			center=false;
-#			digits=64
-#		)::Union{Float64, Tuple{Float64, Array{Float64,1}}}
-#
-#	 - matrixPerturbation(
-#			M::Array{Float64,2};
-#			atol = 1e-10,
-#			row = [0],
-#			col = [0]
-#		)::Array{Float64,2}
-#
-#	 - oppositeHalfSpacePoints(
-#			P::Matrix,
-#			face::Array{Float64,2},
-#			point::Array{Float64,1}
-#		)::Array{Int64,1}
-#
-#	 - planarIntersection(
-#			P::Matrix,
-#			face::Array{Int64,1},
-#			axis::Int64,
-#			off::Float64
-#		)::Int64
-#
-#	 - simplexFaces(
-#			σ::Array{Int64,1}
-#		)::Array{Array{Int64,1},1}
-#
-#	 - vertexInCircumball(
-#			P::Matrix,
-#			α_char::Float64,
-#			point::Array{Float64,2}
-#		)::Bool
-#
-===============================================================================#
-
 """
 	findCenter(P::Matrix)::Array{Float64,1}
 
@@ -138,78 +79,6 @@ function findCenter(P::Matrix)::Array{Float64,1}
 #	AlphaStructures.foundCenter([P[:,i] for i = 1 : size(P, 2)])[:,:]
 end
 
-#-------------------------------------------------------------------------------
-
-"""
-	findClosestPoint(
-		Psimplex::Matrix, P::Matrix;
-		metric = "circumcenter"
-	)::Union{Int64, Nothing}
-
-Returns the index of the closest point in `P` to the `Psimplex` points,
-according to the distance determined by the keyword argument `metric`.
-Possible choices are:
- - `circumcenter`: (default) returns the point that minimize the circumradius
- - `dd`: like `circumcenter` but the circumradius is considered to be negative
-    if the circumcenter is opposite to the new point with respect to `Psimplex`.
-"""
-function findClosestPoint(
-		Psimplex::Matrix, P::Matrix;
-		metric = "circumcenter"
-	)::Union{Int64, Nothing}
-
-	@assert metric ∈ ["circumcenter", "dd"] "findClosestPoint: available metrics are
-		`circumcenter` and `dd`."
-
-	simplexDim = size(Psimplex, 2)
-	@assert simplexDim <= size(Psimplex, 1) "findClosestPoint: Cannot add
-	another point to the simplex."
-
-	@assert (m = size(P, 2)) != 0 "findClosestPoint: No Points in `P`."
-
-	radlist = SharedArray{Float64}(m)
-	@sync @distributed for col = 1 : m
-		r, c = findRadius([Psimplex P[:,col]], true)
-		sameSign = (
-			r == Inf ||
-			metric != "dd" ||
-			isempty(AlphaStructures.oppositeHalfSpacePoints(
-				[Psimplex P[:,col]], Psimplex, c
-			))
-		)
-		radlist[col] = ((-1)^(1 + sameSign)) * r
-	end
-
-	radius, closestidx = findmin(radlist)
-
-	if radius == Inf
-		closestidx = nothing
-	end
-
-	return closestidx
-
-end
-
-#-------------------------------------------------------------------------------
-
-"""
-	findMedian(P::Matrix, ax::Int64)::Float64
-
-Returns the median of the `P` points across the `ax` axis
-"""
-function findMedian(P::Matrix, ax::Int64)::Float64
-	xp = sort(unique(P[ax, :]))
-	if length(xp) == 1
-		median = xp[1]
-	else
-		idx = Int64(floor(length(xp)/2))
-		median = (xp[idx] + xp[idx+1])/2
-	end
-	return median
-end
-
-#-------------------------------------------------------------------------------
-
 """
 	findRadius(
 		P::Matrix, center=false; digits=64
@@ -224,23 +93,6 @@ returns also the circumcenter cartesian coordinates.
 
 _Obs._ Due to numerical approximation errors, the radius is choosen
 as the smallest distance between a point in `P` and the center.
-
-# Examples
-```jldoctest
-
-julia> V = [
- 0.0 1.0 0.0 0.0
- 0.0 0.0 1.0 0.0
- 0.0 0.0 0.0 1.0
-];
-
-julia> AlphaStructures.findRadius(V)
- 0.8660254037844386
-
-julia> AlphaStructures.findRadius(V, true)
- (0.8660254037844386, [0.5, 0.5, 0.5])
-
-```
 """
 function findRadius(
 		P::Matrix, center=false; digits=64
@@ -261,8 +113,6 @@ function findRadius(
 	return r
 end
 
-#-------------------------------------------------------------------------------
-
 """
 	matrixPerturbation(
 		M::Array{Float64,2};
@@ -275,22 +125,6 @@ Returns the matrix `M` with a ±`atol` perturbation on each value determined
 by the `row`-th rows and `col`-th columns.
 If `row` / `col` are set to `[0]` (or not specified) then all the
 rows / columns are perturbated.
-
-# Examples
-```jldoctest
-
-julia> V = [
- 0.0 1.0 0.0 0.0
- 0.0 0.0 1.0 0.0
- 0.0 0.0 0.0 1.0
-];
-
-julia> AlphaStructures.matrixPerturbation(V)
-3×4 Array{Float64,2}:
- -1.27447e-11   1.0           4.68388e-11  3.08495e-11
-  1.17657e-11  -4.25106e-11   1.0          5.62396e-11
-  7.01741e-11  -4.10229e-11  -4.36708e-11  1.0
-
 """
 function matrixPerturbation(
 		M::Array{Float64,2};
@@ -315,164 +149,16 @@ function matrixPerturbation(
 	return N
 end
 
-#-------------------------------------------------------------------------------
-
-"""
-	oppositeHalfSpacePoints(
-			P::Matrix,
-			face::Array{Float64,2},
-			point::Array{Float64,1}
-		)::Array{Int64,1}
-
-Returns the index list of the points `P` located in the halfspace defined by
-`face` points that do not contains the point `point`.
-
-_Obs._ Dimension Dipendent, only works if dimension is three or less and
-	the number of points in the face is the same than the dimension.
-
-# Examples
-```jldoctest
-julia> V = [
- 0.0 1.0 0.0 0.0 4.0 -1. 1.0
- 0.0 0.0 1.0 0.0 1.0 0.0 1.0
- 0.0 0.0 0.0 1.0 2.0 0.0 1.0
-];
-
-julia> oppositeHalfSpacePoints(V, V[:, [2; 3; 4]], V[:, 1])
-2-element Array{Int64,1}:
- 5
- 7
-
-julia> oppositeHalfSpacePoints(V, V[:, [1; 2; 3]], V[:, 4])
-0-element Array{Int64,1}
-
-julia> oppositeHalfSpacePoints(V, V[:, [1; 3; 4]], V[:, 2])
-1-element Array{Int64,1}:
- 6
-
-```
-"""
-function oppositeHalfSpacePoints(
-		P::Matrix,
-		face::Array{Float64,2},
-		point::Array{Float64,1}
-	)::Array{Int64,1}
-
-	dim, n = size(P)
-	noV = size(face, 2)
-	@assert dim <= 3 "oppositeHalfSpacePoints: Not yet coded."
-	@assert noV == dim "oppositeHalfSpacePoints:
-		Cannot determine opposite to non hyperplanes."
-	if dim == 1
-		threshold = face[1]
-		if point[1] < threshold
-			opposite = [i for i = 1 : n if P[1, i] > threshold]
-		else
-			opposite = [i for i = 1 : n if P[1, i] < threshold]
-		end
-	elseif dim == 2
-		if (Δx = face[1, 1] - face[1, 2]) != 0.0
-			m = (face[2, 1] - face[2, 2]) / Δx
-			q = face[2, 1] - m * face[1, 1]
-			# false = under the line, true = over the line
-			@assert point[2] ≠ m * point[1] + q "oppositeHalfSpacePoints,
-				the point belongs to the face"
-			side = sign(m * point[1] + q - point[2])
-			opposite =
-				[i for i = 1 : n if side * (m * P[1, i] + q - P[2, i]) < 0]
-		else
-			q = face[1, 1]
-			side = sign(point[1] - q)
-			opposite = [i for i = 1 : n if side * (P[1, i] - q) < 0]
-		end
-
-
-	elseif dim == 3
-		axis = LinearAlgebra.cross(
-			face[:, 2] - face[:, 1],
-			face[:, 3] - face[:, 1]
-		)
-		off = LinearAlgebra.dot(axis, face[:, 1])
-		position = LinearAlgebra.dot(point, axis)
-		if position < off
-			opposite = [i for i = 1:size(P, 2) if LinearAlgebra.dot(P[:,i], axis) > off]
-		else
-			opposite = [i for i = 1:size(P, 2) if LinearAlgebra.dot(P[:,i], axis) < off]
-		end
-	end
-
-	return [
-		i for i in opposite
-		if sum([P[:, i] == face[:, j] for j = 1 : noV]) == 0
-	]
-end
-
-#-------------------------------------------------------------------------------
-
-"""
-	planarIntersection(
-		P::Matrix,
-		face::Array{Int64,1},
-		axis::Int64,
-		off::Float64
-	)::Int64
-
-Computes the position of `face` with respect to the hyperplane `α` defined by
-the normal `axis` and the contant term `off`. It returns:
- - `+0` if `f` intersect `α` internum (not only the boundary)
- - `+1` if `f` is completely contained in the positive half space of `α`
- - `-1` if `f` is completely contained in the negative half space of `α`
-"""
-function planarIntersection(
-		P::Matrix,
-		face::Array{Int64,1},
-		axis::Int64,
-		off::Float64
-	)::Int64
-
-	pos = [P[axis, i] > off for i in face]
-
-	if sum([P[axis, i] == off for i in face]) == length(pos)
-		position = 0 # face coplanar with axis
-	elseif sum(pos) == 0
-		position = -1
-	elseif sum(pos) == length(pos)
-		position = +1
-	else
-		position = 0
-	end
-
-	return position
-end
-
-#-------------------------------------------------------------------------------
-
 """
 	simplexFaces(σ::Array{Int64,1})::Array{Array{Int64,1},1}
 
 Returns the faces of the simplex `σ`.
 
 _Obs._ The faces are ordered by the index value.
-
-# Examples
-```jldoctest
-
-julia> σ = [1; 3; 2; 4];
-
-julia> AlphaStructures.implexFaces(σ)
-4-element Array{Array{Int64,1},1}:
- [1, 2, 3]
- [1, 2, 4]
- [1, 3, 4]
- [2, 3, 4]
-
-```
 """
 function simplexFaces(σ::Array{Int64,1})::Array{Array{Int64,1},1}
     sort!(sort!.(collect(Combinatorics.combinations(σ, length(σ)-1))))
 end
-
-#-------------------------------------------------------------------------------
 
 """
 	vertexInCircumball(
@@ -482,7 +168,7 @@ end
 	)::Bool
 
 Determine if a point is inner of the circumball determined by `P` points
-	and radius `α_char`.
+and radius `α_char`.
 
 """
 function vertexInCircumball(
